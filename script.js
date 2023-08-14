c26db7e7d707e6606869f27c235b04a80a10dc4d
Sergii Marchenko <marikmarchelo@gmail.com>
2023-08-14 17:19:32 +0300

    new commit

--- script.js
+++ script.js
@@ -2,11 +2,18 @@ function greatWeater(city, days) {
   const BASE_URL = "http://api.weatherapi.com/v1";
   const API_KEY = "6fba6087a507449abdf103545231408";
 
-  fetch(`${BASE_URL}/forecast.json?key=${API_KEY}&q=${city}&days=${days}`).then(
-    (resp) => console.log(resp)
-  );
+  return fetch(
+    `${BASE_URL}/forecast.json?key=${API_KEY}&q=${city}&days=${days}`
+  ).then((resp) => {
+    if (!resp.ok) {
+      throw new Error(resp.statusText);
+    }
+    return resp.json();
+  });
 }
 
-greatWeater("Paris", 5);
+greatWeater("Kiev", 5)
+  .then((error) => console.log(error))
+  .catch((error) => console.log(error));
 
 // http://api.weatherapi.com/v1/forecast.json?key=6fba6087a507449abdf103545231408&q=Paris&days=5
